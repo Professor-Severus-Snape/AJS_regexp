107908635c8d7bff748dd6a7c074fed85ea888bd
Julia Fokanova <oncologmma@mail.ru>
2024-05-15 13:24:24 +0300

    remove parentheses from regexp

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,6 +1,6 @@
 export default class Validator {
   static validateUsername(name) {
-    return (/^[a-z]([\w-])+[a-z]$/i.test(name) && !/\d{4}/.test(name));
+    return (/^[a-z][\w-]+[a-z]$/i.test(name) && !/\d{4}/.test(name));
     // return /^(?!.*\d{4})[a-z][\w-]+[a-z]$/i.test(name);
   }
 }
